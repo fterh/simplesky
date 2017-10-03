ba5f013b512c87ae21c079e7daba715cbb5f1c45
fterh <fabianterh@gmail.com>
2017-10-03 16:30:24 -0700

    10-03(4)
    Implemented AJAX, as well as nowcast and last updated time retrieval

--- simpleskyapp/static/location.js
+++ simpleskyapp/static/location.js
@@ -1,13 +1,28 @@
-
-
 $(document).ready(function() {
 
     // gets location
     // needs refinement in error handling
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(positionAcquired);
+        navigator.geolocation.getCurrentPosition(positionAcquired, showError);
     } else {
-        alert("Gelocation not supported");
+        alert("Geolocation is not supported by this browser; please select location manually");
+    }
+
+    // location errors
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("Unable to get your location; please select location manually");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Unable to get your location; please select location manually");
+                break;
+            case error.TIMEOUT:
+                alert("Unable to get your location; please select location manually");
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("Unknown error; please select location manually");
+        }
     }
 
     // position acquired
@@ -24,9 +39,43 @@ $(document).ready(function() {
 
     // POSTs location
     function postLocation(position) {
-        url = window.location + "ajax/";
-        $.post(url, {lat: position.coords.latitude, long: position.coords.longitude}, function(data) { alert("success!"); });
+        // CSRF
+        var cookieValue = null;
+        var name = "csrftoken";
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    var csrftoken = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        ajax(position, csrftoken);
     }
 
+        // AJAX
+    function ajax(position, csrftoken) {
+        url = window.location + "ajax/";
+        $.ajax({
+            method: "POST",
+            url: url,
+            headers: {"X-CSRFToken": csrftoken},
+            data: {"lat": position.coords.latitude,
+                "long": position.coords.longitude}
+        }).done(function(data) {
+            json = JSON.parse(data)
+            $("#location").html(json["location"]);
+            $("#nowcast").html(json["nowcast"]);
+            $("#lastupdated_2h").html(json["lastupdated_2h"]);
+        })
+    }
 
+    // manual location selection
+        // nowcast
+    $("#select_2h").change(function() {
+        val = $(this).val();
+        
+    })
 })
--- simpleskyapp/views.py
+++ simpleskyapp/views.py
@@ -9,23 +9,22 @@ import pytz
 import requests
 from geopy.distance import vincenty
 
+# declare global variables
+loc_matrix = []
+api = (os.environ.get("API"))
+req_headers = {"api-key": api}
+now = datetime.datetime.now(pytz.timezone("Singapore")). \
+    strftime("%Y-%m-%dT%H:%M:%S")
+
+# 2-hour nowcast
+url_2h = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
+req_payloads = {"date_time": now}
+req_2h = requests.get(url_2h, params=req_payloads, headers=req_headers)
+json_2h = json.loads(req_2h.text)
+
 def index(request):
-    # initialize
-    api = (os.environ.get("API"))
-    req_headers = {"api-key": api}
-    loc_list = ""
-    loc_matrix = []
-    now = datetime.datetime.now(pytz.timezone("Singapore")). \
-        strftime("%Y-%m-%dT%H:%M:%S")
-
-    # 2-hour nowcast
-    url_2h = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
-    req_payloads = {"date_time": now}
-    req_2h = requests.get(url_2h, params=req_payloads, headers=req_headers)
-    json_2h = json.loads(req_2h.text)
-
     # location magic
-    i = 0
+    loc_list = ""
     for area in json_2h["area_metadata"]:
         loc_list += "<option value=\"" + area["name"] + "\">" + area["name"] \
             + "</option>"
@@ -35,8 +34,6 @@ def index(request):
             "lat": area["label_location"]["latitude"],
             "long": area["label_location"]["longitude"]
             })
-
-
 
     #print(json_2h["area_metadata"][0]["name"])
 
@@ -62,5 +59,27 @@ def index(request):
     })
 
 def ajax(request):
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
+    # calculate distances to stations
+    for area in loc_matrix:
+        area_coords = (area["lat"], area["long"])
+        user_coords = (request.POST["lat"], request.POST["long"])
+        distance = vincenty(area_coords, user_coords).miles
+        area["distance"] = distance
+
+    locmatrix_sorted = sorted(loc_matrix, key=lambda x: x["distance"])
+
+    # 2-hour nowcast
+    nowcast, lastupdated_2h = "", ""
+    for area in json_2h["items"][0]["forecasts"]:
+        if area["area"] == locmatrix_sorted[0]["name"]:
+            nowcast = area["forecast"]
+            break
+
+    lastupdated_2h = json_2h["items"][0]["update_timestamp"]
+
+    response = {
+        "location": locmatrix_sorted[0]["name"], \
+        "nowcast": nowcast, \
+        "lastupdated_2h": lastupdated_2h
+        }
+    return HttpResponse(json.dumps(response))
